092addcdd7fe23eb37e194ec4f8e878bd11d6554
MGE <m.gediga02@gmail.com>
2024-06-23 13:59:23 +0200

    Fehler in GetController noch drin

---
+++ src/main/java/com/acme/universitaet/mail/MailProps.java
@@ -0,0 +1,19 @@
+package com.acme.universitaet.mail;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+/**
+ * Spring-Konfiguration für Properties "app.mail.*".
+ *
+ * @author <a href="mailto:Marcel.Gediga@h-ka.de">Marcel Gediga</a>
+ * @param from Emailadresse des Absenders
+ * @param sales Emailadresse des Vertriebs
+ */
+@ConfigurationProperties(prefix = "app.mail")
+public record MailProps(
+    @DefaultValue("Theo Test <theo@test.de>")
+    String from,
+
+    @DefaultValue("Maxi Musterfrau <maxi.musterfrau@test.de>") String sales) {
+}
